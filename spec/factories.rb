1ceb75da9232d5e4b811472c5460e8d902cbbc48
Eli Block <18074026+eliblocks@users.noreply.github.com>
2024-05-04 08:07:38 -0400

    add name to user

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,7 @@
 # This will guess the User class
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.name }
     email { Faker::Internet.email }
     password { SecureRandom.hex }
   end
--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -2,9 +2,9 @@ describe "Signup" do
   it "creates a user" do
     visit new_user_registration_path
 
+    fill_in "Name", with: "John Doe"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "trip_pass"
-    fill_in "Password confirmation", with: "trip_pass"
     click_button "Sign up"
 
     expect(page).to have_content("Welcome!")
